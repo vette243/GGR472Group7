75797a574796f8871a1454bab1570cf8a561e1da
vette243 <liuyiyang1997@hotmail.com>
2023-03-26 16:17:39 -0400

    Buffer added

--- Project_JS.js
+++ Project_JS.js
@@ -127,7 +127,7 @@ map.on('load', () => {
          'layout': {},
          'paint':{
             'fill-opacity': 0.5,
-            'fill-color': '#0080ff'
+            'fill-color': 'green'
          }      
     })
 })
@@ -279,3 +279,98 @@ let layer4check=document.getElementById('Large-Park');
         e.target.checked ? 'visible' : 'none'
     );
 });
+
+//buffer 
+
+//Create empty GeoJSON objects to hold point features
+/*--------------------------------------------------------------------
+STORE USER INPUT FEATURES AS GEOJSON
+--------------------------------------------------------------------*/
+
+// Create empty GeoJSON objects to hold point features
+let geojson = {
+    'type': 'FeatureCollection',
+    'features': []
+};
+
+//Set data source and style on map load
+map.on('load', () => {
+    //Add datasource using GeoJSON variable
+    map.addSource('inputgeojson', {
+        type: 'geojson',
+        data: geojson
+    });
+
+    //Set style for when new points are added to the data source
+    map.addLayer({
+        'id': 'input-pnts',
+        'type': 'circle',
+        'source': 'inputgeojson',
+        'paint': {
+            'circle-radius': 2,
+            'circle-color': 'blue'
+        }
+    });
+
+});
+
+//Add input features to data source based on mouse click and display in map
+map.on('click', (e) => {
+    //Store clicked point as geojson feature
+    const clickedpoint = {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [e.lngLat.lng, e.lngLat.lat]
+        }
+    };
+
+    //Add clicked point to previously empty geojson FeatureCollection variable
+    geojson.features.push(clickedpoint);
+
+    //Update the datasource to include clicked points
+    map.getSource('inputgeojson').setData(geojson);
+
+});
+
+/*--------------------------------------------------------------------
+STORE USER INPUT FEATURES AS GEOJSON
+--------------------------------------------------------------------*/
+document.getElementById('buffbutton').addEventListener('click', () => {
+
+    //Create empty featurecollection for buffers
+    buffresult = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+
+    //Loop through each point in geojson and use turf buffer function to create 0.5km buffer of input points
+    //Add buffer polygons to buffresult feature collection
+    geojson.features.forEach((feature) => {
+        let buffer = turf.buffer(feature, 0.4);
+        buffresult.features.push(buffer);
+    });
+
+    map.addSource('buffgeojson', {
+        "type": "geojson",
+        "data": buffresult  //use buffer geojson variable as data source
+    })
+
+    //Show buffers on map using styling
+    map.addLayer({
+        "id": "inputpointbuff",
+        "type": "fill",
+        "source": "buffgeojson",
+        "paint": {
+            'fill-color': "blue",
+            'fill-opacity': 0.2,
+            'fill-outline-color': "black"
+        }
+    });
+
+    document.getElementById('buffbutton').disabled = true; //disable  button after click
+
+});
+
+
+
